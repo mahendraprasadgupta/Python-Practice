72423f2c01254c5dd10620acbf41591ee6df1d39
Mahendra Prasad Gupta <mpgupta1993@gmail.com>
2021-09-01 23:45:57 +0530

    Lambda Function

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+# Lambda functions
+
+# A Lambda Function is a small, anonymous function — anonymous in the sense that it doesn’t actually have a name.
+# Python functions are typically defined using the style of def a_function_name() , but with lambda functions we don’t give it a name at all. We do this because the purpose of a lambda function is to perform some kind of simple expression or operation without the need for fully defining a function.
+# A lambda function can take any number of arguments, but must always have only one expression:
+
+x = lambda a, b : a * b
+print(x(5, 6)) # prints '30'
+
+x = lambda a : a*3 + 3
+print(x(3)) # prints '12'
